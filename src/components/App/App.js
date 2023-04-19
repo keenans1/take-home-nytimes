19f2eee9c10727b9fd3011b8d55a2f76620a5f61
Keenan Southall <keenan.southall@yahoo.com>
2023-04-18 19:33:11 -0600

    Add fetch call for all automobiles articles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import ArticleList from '../ArticleList/ArticleList';
@@ -7,6 +7,12 @@ import ArticleList from '../ArticleList/ArticleList';
 const  App = () => {
 
   const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.nytimes.com/svc/topstories/v2/automobiles.json?api-key=A2CM4WWPQB2nTtRBnsAojUmnyWQ3jHJY')
+      .then(response => response.json())
+      .then(data => setArticles(data.results))
+  }, [])
 
   return (
     <main className="App">
