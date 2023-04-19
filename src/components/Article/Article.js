7c092a3670a3e7ed3eab36b0ff8454dc6d017348
Keenan Southall <keenan.southall@yahoo.com>
2023-04-18 19:57:21 -0600

    Display article info in each single card

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Article = ({data}) => {
     return (
-        <div>{data.title}</div>
+        <div>
+            <h2>{data.title}</h2>
+            <a href={data.url} target="_blank">Read the article here</a>
+            <h3>{data.byline}</h3>
+        </div>
     )
 }
 
