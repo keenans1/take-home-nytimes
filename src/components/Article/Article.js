1c5ded2a42ab74785f3574a87dba6c2ba045edf2
Keenan Southall <keenan.southall@yahoo.com>
2023-04-20 13:33:53 -0600

    Delete comments

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -3,13 +3,11 @@ import './Article.css'
 import { Link } from "react-router-dom";
 
 const Article = ({data}) => {
-    // console.log('data', data.multimedia[0])
     return (
         <div className="article-card">
             <Link to={`/${data.title}`}>
                 <h2>{data.title}</h2>
                 {data.multimedia ? <img src={data.multimedia[0].url} /> : null}
-                
                 <h3>{data.byline}</h3>
             </Link>
         </div>
